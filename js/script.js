1e77efd643084dad344a9b038de7a66e7b13dae8
Temi <toa5@zips.uakron.edu>
2018-04-20 09:04:58 -0400

    Erasing numbers without using the clean function
    I appended "<" to the "back" function, such that we can delete individual values without clearing everything.

--- js/script.js
+++ js/script.js
@@ -12,3 +12,8 @@ function equal() {
 function clean() {
 	document.form.textview.value = "";
 }
+
+function back() {
+	var exp = document.form.textview.value;
+	document.form.textview.value = exp.substring(0,exp.length-1)
+}
