007404a0c98f4866729cda71fcbb487a4815f4e9
Temi <toa5@zips.uakron.edu>
2018-04-20 08:19:48 -0400

    Adding more js
    Here I added javascript to display individual values in the textarea when clicked on the calculator.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function insert(num) {
 	document.form.textview.value = document.form.textview.value+num;
 }
+
+function equal() {
+	document.form.textview.value;
+	if (exp) {
+		document.form.textview.value = eval(exp);
+	}
+}
